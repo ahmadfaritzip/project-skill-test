cbbaf3cfb474d4417414d0a378a3fb32145f4252
ahmadfaritzip <ahmadfaritzip@gmail.com>
2023-05-23 13:23:13 +0700

    [ADD] file .env

--- resource-api/database/mysqlDB.js
+++ resource-api/database/mysqlDB.js
@@ -1,10 +1,10 @@
 "note strict";
 
 const connection = {
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "test",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 module.exports = connection;
